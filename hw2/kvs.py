69254fc600efa6659ec642107568c16708398a0f
Vincent Moudy <vpmoudy@gmail.com>
2016-04-28 13:11:37 -0700

    added more error catching

--- hw2/kvs.py
+++ hw2/kvs.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
-
-
+import re
+import sys
 app = Flask(__name__)
 
 DATA = {}
@@ -49,8 +49,16 @@ def hello():
 def echo():
     return request.args['msg']
 
+
+@app.route("/kvs/")
+def empty():
+    return handle_noinput_error()
+
 @app.route("/kvs/<key>", methods=['GET', 'PUT', 'DELETE'])
 def foo(key):
+    if len(key) > 250:
+        return handle_keysize_error()
+    
     #request value
     if request.method == 'PUT':
         try:
@@ -60,13 +68,46 @@ def foo(key):
         try:
             value = request.form['val']
         except:
-            get_error
-            
-        return handle_put(key, value)
+            return get_error()
+
+        if sys.getsizeof(value) <= 1573000:
+            if re.match("^[a-zA-Z0-9_]+$", value):
+                return handle_put(key, value) 
+            return handle_char_error()
+        return handle_size_error()
     elif request.method == 'DELETE':
         return handle_delete(key)
+        
     #request.method == GET
     return handle_get(key)
+
+def handle_keysize_error():
+    body = {
+        'msg' : 'error',
+        'error' : 'key size too large'
+    }
+    return jsonify(body), 414
+
+def handle_size_error():
+    body = {
+        'msg' : 'error',
+        'error' : 'input size too large'
+    }
+    return jsonify(body), 413
+
+def handle_noinput_error():
+    body = {
+        'msg' : 'error',
+        'error' : 'no key'
+    }
+    return jsonify(body), 412
+
+def handle_char_error():
+    body = {
+        'msg' : 'error',
+        'error' : 'unsupported character'
+    }
+    return jsonify(body), 413
 
 def handle_put(key, value):
     #key not in dict, create one
@@ -92,4 +133,5 @@ def handle_delete(key):
     return del_success(key)
 
 if __name__ == "__main__":
+    app.debug = True
     app.run(port=8080, host='0.0.0.0')
