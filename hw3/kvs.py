bf3a6f318465e3c68c6c68b40811678839e2528d
Vincent Moudy <vpmoudy@gmail.com>
2016-06-09 13:32:32 -0700

    added environments variables to work in docker

--- hw3/kvs.py
+++ hw3/kvs.py
@@ -4,12 +4,16 @@ import sys
 import os
 import time
 import requests
-import _thread
+import thread
 app = Flask(__name__)
 
-
+addNewData = []
+removeData = []
 DATA = {}
-MEMBERS = [5001, 5002, 5003]
+MEMBERS = os.environ.get('MEMBERS').split(',')
+aliveMembers = []
+afkMembers = []
+deadMembers = []
 initThread = False
 
 
@@ -57,6 +61,9 @@ def hello():
 def echo():
     return request.args['msg']
 
+@app.route("/heartbeat")
+def hbreturn():
+    return myIP
 
 @app.route("/kvs/")
 def empty():
@@ -77,22 +84,24 @@ def primaryHttp(key, method):
         except:
             return get_error()
 
-        if sys.getsizeof(value) <= 1573000:
-            if re.match("^[a-zA-Z0-9_]+$", value):
-                for backup_ip in backupIPs:
-                    r = requests.put(backup_ip + '/backup_kvs/' + key, data = {'val' : value})
-                return handle_put(key, value) 
+        if sys.getsizeof(value) > 1573000:
+            return handle_size_error()
+
+        if not re.match("^[a-zA-Z0-9_]+$", value):
             return handle_char_error()
-        return handle_size_error()
+
+        return handle_put(key, value) 
+            
+       
     #delete message
     elif method == 'DELETE':
-        for backup_ip in backupIPs:
-            r = requests.delete(backup_ip + '/backup_kvs/' + key)
+        
         return handle_delete(key)
     #get message
     return handle_get(key)
    
 def backupHttp(key, method):
+    global primaryIP
     if(method == 'PUT'):
         try:
             value = request.args['val']
@@ -102,8 +111,9 @@ def backupHttp(key, method):
             value = request.form['val']
         except:
             return request.url
-        
-        r = requests.put(primaryIP + '/kvs/' + key, data = {'val' : value})
+        print "Trying to write to: ", primaryIP
+        r = requests.put(primaryIP +  '/kvs/' + key, data = {'val' : value})
+        #r = requests.get(primaryIP + '/hello')
         
         return (r.text, r.status_code, r.headers.items())
 
@@ -129,12 +139,7 @@ def backup_kvs(key):
             value = request.form['val']
         except:
             return get_error()
-
-        if sys.getsizeof(value) <= 1573000:
-            if re.match("^[a-zA-Z0-9_]+$", value):
-                return handle_put(key, value) 
-            return handle_char_error()
-        return handle_size_error()
+        return handle_put(key, value) 
     #delete message
     elif request.method == 'DELETE':
         return handle_delete(key)
@@ -179,6 +184,8 @@ def handle_char_error():
     return jsonify(body), 413
 
 def handle_put(key, value):
+    if primary:
+        addNewData.append((key, value))
     #key not in dict, create one
     if key not in DATA:
         DATA[key] = value
@@ -199,27 +206,113 @@ def handle_delete(key):
     if key not in DATA:
         return del_error(key), 404
     #delete kvs
+    if primary:
+        removeData.append(key)
     return del_success(key)
 
-def sayHello(primary):
-    while (True and not primary):
-        print("I am a thread")
-        time.sleep(5)
-        r = requests.get(primaryIP + '/hello')
+@app.route("/primary_crash")
+def primary_crash():
+    return "Primary crash", 404
+
+@app.route("/alive")
+def alive():
+    return "1", 200
+
+@app.route("/alivecheck/<node>", methods=['GET'])
+def checkNode(node):
+    try:
+        r = requests.get(node + 'alive')
+        return "alive"
+    except (requests.exceptions.ConectionError) as e:
+        return "dead"
+       
+       
+def nodeCrash(node):
+    global aliveMembers
+    global afkMembers
+    global deadMembers
+    global primaryIP
+    global primary
+    global backupIPs
+    aliveMembers.remove(node)
+    print "Alive Nodes: ", aliveMembers
+    afkMembers.append(node)
+    global myIP
+    agreeBool = True
+    for member in aliveMembers:
+        try:
+            r = requests.get(member + '/alivecheck/' + node)
+            if r == "alive":
+                agreeBool = False
+        except (requests.exceptions.ConnectionError) as e:
+            pass
+    if agreeBool == True:
+        afkMembers.remove(node)
+        try:
+            backupIPs.remove(node)
+        except ValueError:
+            pass
+        deadMembers.append(node)
+        print "Dead Nodes: ", deadMembers
+        print "Backups: ", backupIPs
+        primaryIP = aliveMembers[0]
+        if myIP == primaryIP:
+            primary = True
+            try:
+                backupIPs.remove(myIP)
+            except ValueError:
+                pass
+          
+
+#wakes up every 5 secondss to send a heartbeat, then waits
+#1 second for response, if none begin re-election
+def heartbeat():
+    connect_timeout = 1
+    while (True):
+        time.sleep(2)
+        print aliveMembers
+        for node in aliveMembers:
+            if(node == myIP):
+                continue
+            try:
+                r = requests.get(node + '/heartbeat')
+                # sending put request to backups
+                for new_d in addNewData:
+                    for backup_ip in backupIPs:
+                        r = requests.put(backup_ip + '/backup_kvs/' + new_d[0], data = {'val' : new_d[1]})
+                    addNewData.remove(new_d)
+                # sending delete request to backups
+                for rm_data in removeData:
+                    for backup_ip in backupIPs: 
+                        r = requests.delete(backup_ip + '/backup_kvs/' + rm_data)
+                    removeData.remove(rm_data)
+                    #print "text: ", r.text
+            except (requests.exceptions.ConnectionError) as e:
+                #r = requests.get('http://localhost:5002/primary_crash')
+                nodeCrash(node)
+
 
 primary = False
 primaryIP = None
 backupIPs = []
+myIP = None
+myPort = None
 
 if __name__ == "__main__":
     MEMBERS = sorted(MEMBERS)
-    if (int(sys.argv[1]) == MEMBERS[0]):
+    print MEMBERS
+    if ((os.environ.get('IP') + ':' + (os.environ.get('PORT')) == MEMBERS[0])):
         primary = True
    
-    primaryIP = 'http://localhost:' + str(MEMBERS[0])
-    backupIPs.append('http://localhost:' + str(MEMBERS[1]))
-    backupIPs.append('http://localhost:' + str(MEMBERS[2]))
+    primaryIP = 'http://' + str(MEMBERS[0])
+    for member in MEMBERS:
+        backupIPs.append('http://' + member)
+        aliveMembers.append('http://' + member)
+    primaryIP = backupIPs.pop(0)
+    print primaryIP      
     app.debug = False
-    _thread.start_new_thread(sayHello, (primary, ))
+    thread.start_new_thread(heartbeat, ())
+    myPort = os.environ.get('PORT')
+    myIP = 'http://' + os.environ.get('IP') +':' + myPort
 
-    app.run(port=int(sys.argv[1]), host='localhost')
+    app.run(port=myPort, host=os.environ.get('IP'))
